7d4e6101349fa1139f547aa004ce601019a35cf4
ericyuh2001 <62412668+ericyuh2001@users.noreply.github.com>
2020-05-12 18:02:44 +0800

    API更新（工時申請單、員工首頁）
    (1)	DB connection增加DESKTOP-J342AH8\SQLEXPRESS
    (2)	增加 ［EmpID, WorkDate］轉換 ［RowUnid］共用轉換函式
    (3)	增加空資料／無資料前置檢查。
    (4)	GetEmpLeavebyWorkDate測試正常
    (5) 	測試正常

---
+++ HRIS-WAMS-WebCoreAPI/HRIS-WAMS-WebCoreAPI/Models/WhsTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRIS_WAMS_WebCoreAPI.Models
+{
+    public class WhsTable
+    {
+    }
+
+
+
+    [Table("WorkingHours", Schema = "whs")]
+    public class WorkingHoursEntity
+    {
+        [Key]
+        [Required]
+        [Display(Name = "工時填報資料代碼")]
+        public string RowUnid { get; set; }
+
+        [Required]
+        [Display(Name = "員工代號")]
+        public string EmpID { get; set; }
+
+
+        [Required]
+        [Display(Name = "上班日")]
+        public DateTime WorkingDate { get; set; }
+
+
+        [Display(Name = "當日已填報工時")]
+        public decimal WorkingHours { get; set; }
+
+
+        [Display(Name = "應填工時")]
+        public decimal FilledHours { get; set; }
+
+
+        [Display(Name = "是否已填報完成")]
+        public string IsFinish { get; set; }
+
+    }
+
+
+
+
+
+}
